783e5c1693c92710044a4507764a1c8202e053f6
Oluwadarasimi Adeyinka <adeyinkaoluwadarasimi@gmail.com>
2025-12-10 17:06:43 +0100

    change enp to Enp in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 // import { useState } from "react";
 import "./App.css";
 import Envelope from "./components/Envelope";
-import Enp from "./components/enp";
+import Enp from "./components/Enp";
 
 function App() {
   return (
     <>
       <Enp />
       <Envelope />
-      winnner
     </>
   );
 }
